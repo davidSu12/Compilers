1ca25ce7ad9da4b789612e31bbca447561929b44
davidSu12 <david.suarez0501@gmail.com>
2025-07-20 19:53:35 +0200

    [VARIOS] Added simple PDA

---
+++ PDA/ex1/main.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define LNULL NULL
+#define MAX_SIZE 256
+
+
+/*
+    THIS PDA ONLY PARSES THE CFL FORMED BY THE CFG:
+    S -> (S)S
+    S -> ()
+    S -> \epsilon   
+
+*/
+
+struct node{
+    char obj;
+    struct node * next;
+};
+
+typedef struct node * stack;
+
+static stack temp;
+
+
+//a fast and simple implementation of a stack
+void createEmptyStack();
+bool push(char el);
+void pop();
+char peek();
+bool isEmptyStack();
+
+int main(int argc, char **argv){
+
+    char buff[256] = {'\0'};
+
+    fgets(buff, MAX_SIZE, stdin);
+
+    char *com = buff;
+
+    while(*com){
+        if(*com == '('){
+            push('(');
+        }else if(*com == ')'){
+            if(isEmptyStack()){
+                printf("String on bad format\n");
+                return 0;
+            }
+            pop();
+        }
+        com++;
+    }
+
+    if(isEmptyStack()){
+        printf("String on right format\n");
+    }else{
+        printf("String on bad format\n");
+    }
+
+    return 0;
+}
+
+
+void createEmptyStack(){
+    temp = LNULL;
+}
+bool isEmptyStack(){
+    return temp == LNULL;
+}
+
+bool push(char el){
+    struct node *elemento = malloc(sizeof(struct node));
+    if(!elemento) {
+        fprintf(stderr, "Malloc error");
+        exit(EXIT_FAILURE);
+    }
+    elemento -> obj = el;
+    elemento -> next = LNULL;
+    if(isEmptyStack()){
+        temp = elemento;
+    }else{
+        elemento -> next = temp;
+        temp = elemento;
+    }
+    return true;
+}
+
+void pop(){
+    if(isEmptyStack()){
+        fprintf(stderr, "Empty stack");
+    }else{
+         struct node * t = temp;
+         temp = temp -> next;
+         free(temp);
+    };
+}
+
+
+char peek(){
+    if(isEmptyStack()){
+        fprintf(stderr, "EmptyStack");
+    }else{
+        return temp -> obj;
+    }
+}
+
